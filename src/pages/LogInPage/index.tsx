8e497f00f668fd542c70302e30fbd6d77a7f6d88
Jefferson Moraes <jeff.dev.moraes@gmail.com>
2020-08-11 13:31:59 +0200

    LogIn screen structure/style

--- src/pages/LogInPage/index.tsx
+++ src/pages/LogInPage/index.tsx
@@ -1,15 +1,114 @@
 import React from "react";
-import { View, Text, TouchableOpacity } from "react-native";
+import { View, Image, Text, TouchableOpacity, StyleSheet } from "react-native";
+
+const logo = require("../../img/krds-logo.svg");
 
 const LogInPage = ({ navigation }) => {
   return (
     <View>
-      <Text>LogIn</Text>
-      <TouchableOpacity onPress={() => navigation.goBack()}>
-        <Text>Go back</Text>
-      </TouchableOpacity>
+      <View style={styles.container}>
+        <Image source={logo} style={styles.logo} />
+        <Text style={styles.title}>LOGIN</Text>
+      </View>
+      <View style={styles.containerRow}>
+        <View style={styles.leftBox} />
+        <TouchableOpacity
+          style={styles.loginButton}
+          onPress={() => navigation.navigate("LogIn")}
+        >
+          <Text style={styles.loginText}>LOGIN</Text>
+        </TouchableOpacity>
+      </View>
+
+      <View style={styles.containerRow}>
+        <View style={styles.emptyLeftBox} />
+        <TouchableOpacity
+          style={styles.signupButton}
+          onPress={() => navigation.goBack()}
+        >
+          <Text style={styles.signupText}>GO BACK</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop: 70,
+    marginLeft: 50,
+    borderTopColor: "#BF2D5B",
+    borderLeftColor: "#BF2D5B",
+    borderTopLeftRadius: 50,
+    borderLeftWidth: 1,
+    borderTopWidth: 1,
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  logo: {
+    marginTop: 70,
+    height: 123,
+    width: 172,
+  },
+  title: {
+    fontSize: 24,
+    color: "#61468F",
+    alignSelf: "flex-start",
+    marginTop: 50,
+    marginBottom: 30,
+    marginLeft: 35,
+  },
+  containerRow: {
+    display: "flex",
+    flexDirection: "row",
+  },
+  emptyLeftBox: {
+    height: 50,
+    width: 50,
+  },
+  leftBox: {
+    borderTopColor: "#962623",
+    borderTopWidth: 1,
+    borderBottomColor: "#962623",
+    borderBottomWidth: 1,
+    width: 50,
+    height: 50,
+  },
+  loginButton: {
+    flex: 1,
+    height: 50,
+    borderTopColor: "#7C1A38",
+    borderTopWidth: 1,
+    borderBottomColor: "#7C1A38",
+    borderBottomWidth: 1,
+    borderLeftColor: "#CF6F3A",
+    borderLeftWidth: 1,
+
+    textAlign: "right",
+    padding: 15,
+    paddingRight: 50,
+  },
+  loginText: {
+    color: "#7C1A38",
+  },
+  signupButton: {
+    flex: 1,
+    height: 50,
+    borderBottomColor: "#CF6F3A",
+    borderBottomWidth: 1,
+    borderLeftColor: "#CF6F3A",
+    borderLeftWidth: 1,
+    borderBottomLeftRadius: 40,
+
+    textAlign: "right",
+    padding: 15,
+    paddingRight: 50,
+  },
+  signupText: {
+    color: "#CF6F3A",
+  },
+});
 
 export default LogInPage;
